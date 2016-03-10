119d5a5bad169c75e4ff11d353ee585a379f42a1
speed <k78@qq.com>
2016-03-10 10:38:04 +0800

    Update install_x86_64.sh
    add download url

--- install_x86_64.sh
+++ install_x86_64.sh
@@ -6,12 +6,12 @@ cd newcrawler
 
 yum -y install tar
 
-#jetty
+#jetty http://download.eclipse.org/jetty/
 wget --no-check-certificate http://download.eclipse.org/jetty/9.3.6.v20151106/dist/jetty-distribution-9.3.6.v20151106.tar.gz
 tar zxvf jetty-distribution-9.3.6.v20151106.tar.gz
 mv jetty-distribution-9.3.6.v20151106 jetty
 
-#jre
+#jre http://www.oracle.com/technetwork/java/javase/downloads/index.html
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u66-b17/server-jre-8u66-linux-x64.tar.gz
 tar zxvf server-jre-8u66-linux-x64.tar.gz
 mv jdk1.8.0_66/jre jre
@@ -24,7 +24,7 @@ unzip -n master.zip
 mv newcrawler-master/war war
 rm -R -f -v newcrawler-master
 
-#PhantomJs
+#PhantomJs http://phantomjs.org/download.html
 yum -y install bzip2
 yum -y install fontconfig freetype libfreetype.so.6 libfontconfig.so.1
 wget --no-check-certificate https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-1.9.8-linux-x86_64.tar.bz2
