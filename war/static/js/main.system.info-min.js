04660807f0ce5ab58e80ea624f474c7c7650eed4
speed <k78@qq.com>
2016-06-25 16:32:41 +0800

    Permissions Cache Bug fix

--- war/static/js/main.system.info-min.js
+++ war/static/js/main.system.info-min.js
@@ -1,1 +1,1 @@
-var systemInfo={v:{tableId:"systemInfoTable",webCrawlerId:null},fn:{detailInfo:function(){jsonrpc.systemService.info(function(a,b,c){removeLoading($("#"+systemInfo.v.tableId));$("#"+systemInfo.v.tableId).find(".detailInfo").html(a)},systemInfo.v.webCrawlerId)}}};
+var systemInfo={v:{tableId:"systemInfoTable",webCrawlerId:null},fn:{detailInfo:function(){jsonrpc.systemService.info(function(a,b,c){removeLoading($("#"+systemInfo.v.tableId));if(b){return}$("#"+systemInfo.v.tableId).find(".detailInfo").html(a)},systemInfo.v.webCrawlerId)}}};
