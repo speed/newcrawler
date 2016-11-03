dcfbb16b4220f5b725d37e89aca495abc2bad559
speed <k78@qq.com>
2016-11-03 17:02:12 +0800

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -1,15 +1,15 @@
 #upgrade
 
 cd newcrawler
-rm -f -v master.zip
-wget --no-check-certificate https://github.com/speed/newcrawler/archive/master.zip -O master.zip
-unzip -n master.zip
-rm -R -f -v newcrawler-master/war/WEB-INF/classes
-rm -R -f -v war/WEB-INF/lib
-rm -R -f -v war/static
-\cp -R newcrawler-master/war/* war
-rm -R -f -v newcrawler-master
-rm -f -v master.zip
+
+#bak
+\cp -R war/WEB-INF/classes war/WEB-INF/classes_bak
+
+git pull
+
+\cp -R war/WEB-INF/classes_bak war/WEB-INF/classes
+rm -R -f -v war/WEB-INF/classes_bak
+
 sh stop.sh
 
 sh start.sh &
