71e52d423ab95ec26fb5e994c30ba55b4af281ba
speed <k78@qq.com>
2016-11-03 16:54:45 +0800

    Update install_x86_64.sh

--- install_x86_64.sh
+++ install_x86_64.sh
@@ -1,16 +1,16 @@
 #install.sh
-#jetty http://download.eclipse.org/jetty/
+#jetty https://www.eclipse.org/jetty/download.html
 #jre http://www.oracle.com/technetwork/java/javase/downloads/index.html
 #PhantomJs http://phantomjs.org/download.html
-jetty="http://download.eclipse.org/jetty/9.3.7.v20160115/dist/jetty-distribution-9.3.7.v20160115.tar.gz"
+jetty="http://repo1.maven.org/maven2/org/eclipse/jetty/jetty-distribution/9.3.13.v20161014/jetty-distribution-9.3.13.v20161014.tar.gz"
 jre="http://download.oracle.com/otn-pub/java/jdk/8u74-b02/server-jre-8u74-linux-x64.tar.gz"
 phantomjs="https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2"
 
-mkdir newcrawler
+yum -y install tar git
+
+git clone https://github.com/speed/newcrawler.git
+
 cd newcrawler
-
-
-yum -y install tar
 
 #jetty
 wget --no-check-certificate $jetty -O jetty.tar.gz
@@ -20,13 +20,6 @@ mkdir ./jetty && tar -xzvf jetty.tar.gz -C ./jetty --strip-components 1
 #jre
 wget --no-check-certificate --no-cookies --header "Cookie: oraclelicense=accept-securebackup-cookie" $jre -O server-jre-linux.tar.gz
 mkdir ./jre && tar -xzvf server-jre-linux.tar.gz -C ./jre --strip-components 1
-
-#war
-yum -y install unzip
-wget --no-check-certificate https://github.com/speed/newcrawler/archive/master.zip -O master.zip
-unzip -n master.zip
-mv newcrawler-master/war war
-rm -R -f -v newcrawler-master
 
 #PhantomJs
 yum -y install bzip2
@@ -44,6 +37,5 @@ wget --no-check-certificate https://github.com/speed/linux-64bit-jetty-jre/raw/m
 rm -f -v jetty.tar.gz
 rm -f -v phantomjs-linux.tar.bz2
 rm -f -v server-jre-linux.tar.gz
-rm -f -v master.zip
 
 echo 'Congratulations, the installation is successful.'
