f64cf80ac771850c93990fa1634343a0cf98ad7c
speed <k78@qq.com>
2017-01-11 11:43:15 +0800

    Update install_x86_64.sh

--- install_x86_64.sh
+++ install_x86_64.sh
@@ -8,7 +8,7 @@ phantomjs="https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux
 
 yum -y install tar git
 
-git clone https://github.com/speed/newcrawler.git
+git clone https://github.com/speed/newcrawler.git ~/newcrawler
 
 cd newcrawler
 
