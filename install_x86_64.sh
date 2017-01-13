26f28af1118cfd9e2f54e710775eb00e67cd2da1
speed <k78@qq.com>
2017-01-13 15:52:56 +0800

    Update install_x86_64.sh

--- install_x86_64.sh
+++ install_x86_64.sh
@@ -41,6 +41,7 @@ mkdir ./phantomjs && tar -xjvf phantomjs-linux.tar.bz2 -C ./phantomjs --strip-co
 phantomjs/bin/phantomjs --version
 
 #Script and Config
+rm -f -v start.sh
 wget --no-check-certificate https://github.com/speed/linux-64bit-jetty-jre/raw/master/jetty/webapps/newcrawler.xml -P jetty/webapps/ -O jetty/webapps/newcrawler.xml
 wget --no-check-certificate https://github.com/speed/linux-64bit-jetty-jre/raw/master/start.sh -O start.sh
 wget --no-check-certificate https://github.com/speed/linux-64bit-jetty-jre/raw/master/stop.sh -O stop.sh
