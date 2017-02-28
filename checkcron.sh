c54a506f189c08088cf657ec2d8eac4f0050c004
speed <k78@qq.com>
2017-02-28 11:57:04 +0800

    Update checkcron.sh

--- checkcron.sh
+++ checkcron.sh
@@ -1,6 +1,9 @@
 #install crontab
 yum install cronie vixie-cron -y
 
+chkconfig crond on
+service crond start
+
 #every 2 minutes
 chmod +x ~/newcrawler/check.sh
 (crontab -l ; echo "*/2 * * * * ~/newcrawler/check.sh") | crontab -
