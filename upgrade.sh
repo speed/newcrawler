29978819ee1d741e1d86e46b9784991fd4d1ef8c
speed <k78@qq.com>
2018-03-13 12:01:21 +0800

    update

--- upgrade.sh
+++ upgrade.sh
@@ -1,29 +1,4 @@
 #upgrade
+git checkout update.sh
 
-cd ~/newcrawler
-
-#bak
-mkdir war/WEB-INF/classes_bak
-\cp war/WEB-INF/classes/config.properties war/WEB-INF/classes_bak/config.properties
-\cp war/WEB-INF/classes/datanucleus.properties war/WEB-INF/classes_bak/datanucleus.properties
-
-git checkout war/WEB-INF/classes
-git pull
-
-rm -ivf install_*.sh
-rm -ivf Dockerfile
-
-\cp war/WEB-INF/classes_bak/config.properties war/WEB-INF/classes/config.properties
-\cp war/WEB-INF/classes_bak/datanucleus.properties war/WEB-INF/classes/datanucleus.properties
-
-
-rm -ivfR war/WEB-INF/classes_bak
-
-sh stop.sh
-
-#error='Cannot allocate memory'
-pkill java
-
-pkill chrome
-
-sh start.sh &
+sh update.sh
