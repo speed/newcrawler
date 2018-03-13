684a299d73ec9bbf7eaa7e91be114ad52ccf2b35
speed <k78@qq.com>
2018-03-13 15:01:52 +0800

    update

--- update.sh
+++ update.sh
@@ -5,7 +5,6 @@ cd ~/newcrawler
 #bak
 mkdir war/WEB-INF/classes_bak
 \cp war/WEB-INF/classes/config.properties war/WEB-INF/classes_bak/config.properties
-\cp war/WEB-INF/classes/datanucleus.properties war/WEB-INF/classes_bak/datanucleus.properties
 
 git checkout war/WEB-INF/classes
 git pull
@@ -14,7 +13,6 @@ rm -ivf install_*.sh
 rm -ivf Dockerfile
 
 \cp war/WEB-INF/classes_bak/config.properties war/WEB-INF/classes/config.properties
-\cp war/WEB-INF/classes_bak/datanucleus.properties war/WEB-INF/classes/datanucleus.properties
 
 
 rm -ivfR war/WEB-INF/classes_bak
