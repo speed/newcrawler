c0ed7436763321e7c0bcdbbb617580d27eff923c
speed <k78@qq.com>
2018-06-13 02:54:36 +0800

    Update install_x86_64.sh

--- install_x86_64.sh
+++ install_x86_64.sh
@@ -6,7 +6,7 @@
 #PhantomJs http://phantomjs.org/download.html
 jetty="http://repo1.maven.org/maven2/org/eclipse/jetty/jetty-distribution/9.3.13.v20161014/jetty-distribution-9.3.13.v20161014.tar.gz"
 #jre="http://download.oracle.com/otn-pub/java/jdk/8u172-b11/a58eab1ec242421181065cdc37240b08/server-jre-8u172-linux-x64.tar.gz"
-jre="https://github.com/speed/jdk/raw/master/jdk8u172/server-jre-8u172-linux-x64.tar.gz"
+jre="https://github.com/speed/newcrawler-dependency/raw/master/jdk8u172/server-jre-8u172-linux-x64.tar.gz"
 phantomjs="https://bitbucket.org/ariya/phantomjs/downloads/phantomjs-2.1.1-linux-x86_64.tar.bz2"
 
 yum -y install tar git
