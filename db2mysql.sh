b2d01ab9233426c89455e5459ea986c87f11acb9
speed <k78@qq.com>
2019-01-18 11:49:15 +0800

    Update db2mysql.sh

--- db2mysql.sh
+++ db2mysql.sh
@@ -15,8 +15,8 @@ systemctl enable mariadb
 
 mysql --default-character-set=utf8 < ~/newcrawler/newcrawler.sql
 
-sudo sed -i "s/.*javax.jdo.option.ConnectionDriverName.*/javax.jdo.option.ConnectionDriverName = com.mysql.jdbc.Driver/" ~/newcrawler/war/WEB-INF/classes/config.properties
-sudo sed -i "s/.*javax.jdo.option.ConnectionURL.*/javax.jdo.option.ConnectionURL=jdbc:mysql://127.0.0.1:3306/newcrawler?characterEncoding=UTF-8/" ~/newcrawler/war/WEB-INF/classes/config.properties
+sudo sed -i "s/.*javax.jdo.option.ConnectionDriverName.*/javax.jdo.option.ConnectionDriverName=com.mysql.jdbc.Driver/" ~/newcrawler/war/WEB-INF/classes/config.properties
+sudo sed -i "s/.*javax.jdo.option.ConnectionURL.*/javax.jdo.option.ConnectionURL=jdbc:mysql:\/\/127.0.0.1:3306\/newcrawler?characterEncoding=UTF-8/" ~/newcrawler/war/WEB-INF/classes/config.properties
 sudo sed -i "s/.*javax.jdo.option.ConnectionUserName.*/javax.jdo.option.ConnectionUserName=root/" ~/newcrawler/war/WEB-INF/classes/config.properties
 sudo sed -i "s/.*javax.jdo.option.ConnectionPassword.*/javax.jdo.option.ConnectionPassword=/" ~/newcrawler/war/WEB-INF/classes/config.properties
 sudo sed -i "s/.*javax.jdo.option.Mapping.*/javax.jdo.option.Mapping=mysql/" ~/newcrawler/war/WEB-INF/classes/config.properties
