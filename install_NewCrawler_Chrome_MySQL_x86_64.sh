9b6099e9f95a2ab9f41f51cb42ba099403d7b15e
speed <k78@qq.com>
2019-01-25 10:11:24 +0800

    Update install_NewCrawler_Chrome_MySQL_x86_64.sh

--- install_NewCrawler_Chrome_MySQL_x86_64.sh
+++ install_NewCrawler_Chrome_MySQL_x86_64.sh
@@ -2,15 +2,20 @@
 
 yum -y install wget
 
+#install NewCrawler
 wget --no-check-certificate --no-verbose https://raw.githubusercontent.com/speed/newcrawler/master/install_x86_64.sh -O NewCrawler.sh \
   && sh NewCrawler.sh
-  
+
+#install Chrome
 wget --no-check-certificate --no-verbose https://raw.githubusercontent.com/speed/selenium/master/Centos.sh -O Selenium-Chrome.sh \
   && sh Selenium-Chrome.sh
-  
+
+#install MySQL
 sh newcrawler/db2mysql.sh
-  
+
+#start NewCrawler
 sh newcrawler/start.sh
 
+#add killed cron
 sh newcrawler/checkcron.sh
 
