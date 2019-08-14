4bf7f5f5c5e0c608df1bcc2bc10c36074db32803
speed <k78@qq.com>
2019-08-14 21:55:51 +0800

    update

--- upgrade.sh
+++ upgrade.sh
@@ -5,4 +5,6 @@ git fetch
 git checkout origin/master -- update.sh
 git checkout origin/master -- upgrade.sh
 
+git checkout check.sh check-docker-chrome.sh
 sh update.sh
+chmod +x  check.sh check-docker-chrome.sh
